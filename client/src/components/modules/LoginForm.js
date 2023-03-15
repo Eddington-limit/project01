336cf5ce4cd703c0a034eb4bb42eee2541a144d8
Sam <jiaxixian@hotmail.com>
2023-03-15 10:23:08 +0800

    login form

--- client/src/components/modules/LoginForm.js
+++ client/src/components/modules/LoginForm.js
@@ -12,8 +12,16 @@ function LoginForm() {
         setIsVisible(!isVisible);
     }
 
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value)
+    }
+
     const loginRequest = () => {
-        post("api/login",{})
+        post("api/login",{username:username,password:password})
     }
 
   return (
@@ -22,8 +30,8 @@ function LoginForm() {
         <button onClick={toggleVisibility}>x</button>}
       {isVisible && (
         <div>
-          <input type="text" placeholder="Username" onChange={handleChange} />
-          <input classname="dropdown" type="password" placeholder="Password" />
+          <input type="text" placeholder="Username" onChange={handleUsernameChange} />
+          <input classname="dropdown" type="password" placeholder="Password" onChange={handlePasswordChange} />
           <button onClick={loginRequest}>Login</button>
         </div>
       )}
